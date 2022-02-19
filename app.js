2d4ba13ea14b5447c6b570a5849f488d4ef2f8ad
sangyun lee <sororiri@gmail.com>
2022-02-19 19:31:12 +0900

    feat : Add page that create new Daily.

--- app.js
+++ app.js
@@ -2,27 +2,69 @@
 const express = require("express");
 const ejsMate = require('ejs-mate');
 const path = require('path');
+const methodOverride = require('method-override');
 
 // express app setting
 const app = express();
 
 // set view settings
-
 app.set('view engine', 'ejs');
 app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.render('home.ejs');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String,
+    description: String
+});
+const ImageModel = mongoose.model('Image', ImageSchema);
+
+app.get('/', async (req, res) =>  {
+    const imgArray = await ImageModel.find({});
+    res.render('home', {imgArray});
+})
+
+app.get('/new', (req, res) => {
+    res.render('new');
+});
+
+app.post('/', async (req, res) => {
+    console.dir(req.body);
+    const body = req.body;
+    const img = new ImageModel({filename: body.title, description: body.description, url: body.url });
+    await img.save();
+    res.redirect(`/`);
+});
+
+app.get('/:id', async (req, res) =>  {
+    try {
+        const img = await ImageModel.findById(req.params.id);
+        if (!img) {
+            return res.redirect('/');
+        }
+        console.dir(img);
+        res.render('show', { img });
+    } catch (error) {
+        return res.redirect('/');
+    }
 })
 
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+    console.log("DB connected");
+});
 
 
-
-
-
+const dbUrl = "mongodb://localhost:27017/touch-world";
+mongoose.connect(dbUrl, {
+});
 
 
 
