97f580e67ef6196b1e2a7a20f822ffd63b74772c
leesangyun <sylee@ivisolution.com>
2022-05-19 17:55:17 +0900

    remove all to migration to react

--- middleware.js
+++
@@ -1,9 +0,0 @@
-const requireLogin = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-        req.session.returnUrl = req.originalUrl;
-        return res.redirect('/user/login');
-    }
-    next();
-};
-
-module.exports.requireLogin = requireLogin;
--- utils/catchAsync.js
+++
@@ -1,5 +0,0 @@
-module.exports.catchAsync = func => {
-    return (req, res, next) => {
-        func(req, res, next).catch(next);
-    }
-}
