5a00c32aa3f9b3452b09e45ccbcd8d516a006831
leesangyun <sylee@ivisolution.com>
2022-05-19 17:59:28 +0900

    init react

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './component/Navbar'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
