781bc97b162f6fc9fcf09d5b640fda4d675cc6cf
sangyun lee <sororiri@gmail.com>
2022-12-03 12:13:23 +0900

    [ 이상윤] 로그인 후 24시간이후에도 로그인 토큰 값이 사라지지 않도록 변경

--- backend/app/controller/auth.controller.ts
+++ backend/app/controller/auth.controller.ts
@@ -78,7 +78,7 @@ export const signin = (req, res) => {
         });
       }
       var token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400, // 24 hours
+        // expiresIn: 86400 * 7, // 24 hours
       });
       var authorities = [];
       for (let i = 0; i < user.roles.length; i++) {
